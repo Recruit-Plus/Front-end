9200cae69867cb329935fde67c38b821875f4a35
vibhutidhande <83495237+vibhutidhande@users.noreply.github.com>
2022-07-26 17:29:30 +0530

    HomePage for Recruit+

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
+    <Navbar/>
+    <HomePage/>
     </>
   );
 }
